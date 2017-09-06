0fcb2b630f010e23e92cdc0346ae70abde8a5d84
Joaquin Gayoso Cabada <jgayoso@ucm.es>
2017-09-06 18:36:12 +0200

    Vamos avanzando poco a poco

--- GMapsURL/src/fdi/maps/client/GMapsEJ.java
+++ GMapsURL/src/fdi/maps/client/GMapsEJ.java
@@ -5,14 +5,16 @@ import java.util.LinkedList;
 import com.google.gwt.core.client.Callback;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.JsArray;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.geolocation.client.Geolocation;
 import com.google.gwt.geolocation.client.Position;
 import com.google.gwt.geolocation.client.PositionError;
+import com.google.gwt.http.client.Request;
+import com.google.gwt.http.client.RequestBuilder;
+import com.google.gwt.http.client.RequestCallback;
+import com.google.gwt.http.client.RequestException;
+import com.google.gwt.http.client.Response;
 import com.google.gwt.geolocation.client.Position.Coordinates;
 import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.FormPanel;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.maps.gwt.client.Geocoder;
@@ -32,23 +34,27 @@ import fdi.maps.shared.ConstantsGeoLocal;
 
 
 public class GMapsEJ implements EntryPoint {
+	protected static final String ERROR_SETTING_DATA= "Error sending data to edition, try again and test asociated systems";
 	LinkedList<Marker> listaMarked;
 	private Geocoder fCoder;
 	private GoogleMap gMap;
 	private Marker ActualMarked;
 	
-	private String PUNTO_NO_SETEADO="Point not seted yet, click on map to set the position";
+//	private String PUNTO_NO_SETEADO="Point not seted yet, click on map to set the position";
 	private double DLatitude;
 	private double DLongitude;
 	private FormPanel panel;
 	private boolean editableAction;
 	private String postUrl;
 	private String passId;
+	private String protocol;
 	
 	public void onModuleLoad() {
 		
 		
 		//Captura de Parametros http://localhost:8080/GMapsURL/?latitude=36.5008762&longitude=-6.2684345
+		
+		Window.alert("Hola");
 		
 		Coordinates P=null;
 		
@@ -57,7 +63,16 @@ public class GMapsEJ implements EntryPoint {
 		String passlongitude = com.google.gwt.user.client.Window.Location.getParameter(ConstantsGeoLocal.LONGITUDE);
 		String edit = com.google.gwt.user.client.Window.Location.getParameter(ConstantsGeoLocal.EDIT);
 		postUrl = com.google.gwt.user.client.Window.Location.getParameter(ConstantsGeoLocal.POSTURL);
+		protocol = com.google.gwt.user.client.Window.Location.getParameter(ConstantsGeoLocal.PROTOCOL);
 		
+		if (protocol!=null)
+			{
+			protocol=protocol.toLowerCase();
+			if (!protocol.toLowerCase().equals("http")||protocol.toLowerCase().equals("https"))
+				protocol="http";
+			}
+		else
+			protocol="http";
 		
 		
 		try {
@@ -195,6 +210,7 @@ public class GMapsEJ implements EntryPoint {
 
 	protected void processMap(Coordinates coor) {
         panel = new FormPanel();
+        panel.setMethod(FormPanel.METHOD_GET);
         panel.setWidth("100%");
         panel.setHeight("600px");
         MapOptions options = MapOptions.create();
@@ -253,13 +269,18 @@ public class GMapsEJ implements EntryPoint {
 							 if (postUrl!=null&&!postUrl.isEmpty())
 							 {
 							
-							String URLPOSTF = postUrl+"?"+ConstantsGeoLocal.LATITUDE+"="+ActualMarked.getPosition().lat()+"&"+ConstantsGeoLocal.LONGITUDE+"="+ActualMarked.getPosition().lng();
+							String URLPOSTF = protocol+"://"+postUrl+"?"+ConstantsGeoLocal.LATITUDE+"="+ActualMarked.getPosition().lat()+"&"+ConstantsGeoLocal.LONGITUDE+"="+ActualMarked.getPosition().lng();
 							
 							if (passId!=null&&!passId.isEmpty())
 								URLPOSTF=URLPOSTF+"&"+ConstantsGeoLocal.PASSID+"="+passId;
+//							Window.alert(URLPOSTF);
+//							panel.setAction(URLPOSTF);
+//							panel.submit();
 							
-							panel.setAction(URLPOSTF);
-							panel.submit();
+							doGet(URLPOSTF);
+							
+
+							
 							 }
 							
 					        
@@ -271,12 +292,33 @@ public class GMapsEJ implements EntryPoint {
         }
 
 
+        
+        
 
 		
 	}
 	
 	
+	public void doGet(String url) {
+        RequestBuilder builder = new RequestBuilder(RequestBuilder.GET, url);
 
+        try {
+            builder.sendRequest(null, new RequestCallback() {
+            	
+                public void onError(Request request, Throwable exception) {
+                    // Code omitted for clarity
+                	Window.alert(ERROR_SETTING_DATA+" to " +postUrl+"->"+exception.getCause());
+                }
+
+                public void onResponseReceived(Request request, Response response) {
+                    // Code omitted for clarity
+                }
+            });
+
+        } catch (RequestException e) {
+            // Code omitted for clarity
+        }
+    }
 	
 	
 	
--- GMapsURL/src/fdi/maps/shared/ConstantsGeoLocal.java
+++ GMapsURL/src/fdi/maps/shared/ConstantsGeoLocal.java
@@ -6,6 +6,10 @@ public class ConstantsGeoLocal {
 	public static final String LONGITUDE = "longitude";
 	public static final String ICONPATH = "gmaps.png";
 	public static final String EDIT="edit";
-	public static final String POSTURL="posturl";
-	public static final String PASSID = "passid";
+	
+	
+	//NECESARIAS EN LOS ENVIOS PARA PODER EDITAR
+		public static final String POSTURL="posturl";
+		public static final String PASSID = "passid";
+		public static final String PROTOCOL = "protocol";
 }
